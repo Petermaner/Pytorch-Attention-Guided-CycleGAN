b5a8200ad2ac07cbabdf6ed63ab6b0a0332978ab
Petermaner <30564542+Petermaner@users.noreply.github.com>
2022-04-08 20:27:22 +0800

    Update models.py

--- models.py
+++ models.py
@@ -36,14 +36,14 @@ class Gen(nn.Module):
         
         model = [   nn.ReflectionPad2d(3),
             nn.Conv2d(input_nc, 32, 7),
-            nn.ReLU(inplace=True) ]
+            nn.ReLU(inplace=False) ]
 
         # Downsampling
         in_features = 32
         out_features = in_features*2
         for _ in range(2):
             model += [  nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
-                        nn.ReLU(inplace=True) ]
+                        nn.ReLU(inplace=False) ]
             in_features = out_features
             out_features = in_features*2
 
@@ -55,7 +55,7 @@ class Gen(nn.Module):
         out_features = in_features//2
         for _ in range(2):
             model += [  nn.ConvTranspose2d(in_features, out_features, 3, stride=2, padding=1, output_padding=1),
-                        nn.ReLU(inplace=True) ]
+                        nn.ReLU(inplace=False) ]
             in_features = out_features
             out_features = in_features//2
 
@@ -78,19 +78,19 @@ class Dis(nn.Module):
         super(Dis, self).__init__()
 
         model = [   nn.Conv2d(input_nc, 64, 4, stride=2, padding=1),
-                    nn.LeakyReLU(0.2, inplace=True) ]
+                    nn.LeakyReLU(0.2, inplace=False) ]
 
         model += [  nn.Conv2d(64, 128, 4, stride=2, padding=1),
                     nn.InstanceNorm2d(128), 
-                    nn.LeakyReLU(0.2, inplace=True) ]
+                    nn.LeakyReLU(0.2, inplace=False) ]
 
         model += [  nn.Conv2d(128, 256, 4, stride=2, padding=1),
                     nn.InstanceNorm2d(256), 
-                    nn.LeakyReLU(0.2, inplace=True) ]
+                    nn.LeakyReLU(0.2, inplace=False) ]
 
         model += [  nn.Conv2d(256, 512, 4, padding=1),
                     nn.InstanceNorm2d(512), 
-                    nn.LeakyReLU(0.2, inplace=True) ]
+                    nn.LeakyReLU(0.2, inplace=False) ]
 
         # FCN classification layer
         model += [nn.Conv2d(512, 1, 4, padding=1)]
@@ -112,11 +112,11 @@ class Attn(nn.Module):
 
         model =  [  nn.Conv2d(3, 32, 7, stride=1, padding=3),
                     nn.InstanceNorm2d(32),
-                    nn.ReLU(inplace=True) ]
+                    nn.ReLU(inplace=False) ]
 
         model += [  nn.Conv2d(32, 64, 3, stride=2, padding=1),
                     nn.InstanceNorm2d(64),
-                    nn.ReLU(inplace=True) ]
+                    nn.ReLU(inplace=False) ]
 
         model += [ResBlock(64, norm=True)]
 
@@ -124,13 +124,13 @@ class Attn(nn.Module):
 
         model += [  nn.Conv2d(64, 64, 3, stride=1, padding=1),
                     nn.InstanceNorm2d(64),
-                    nn.ReLU(inplace=True) ]
+                    nn.ReLU(inplace=False) ]
         
         # model += [nn.UpsamplingNearest2d(scale_factor=2)]
 
         model += [  nn.Conv2d(64, 32, 3, stride=1, padding=1),
                     nn.InstanceNorm2d(32),
-                    nn.ReLU(inplace=True) ]
+                    nn.ReLU(inplace=False) ]
 
         model += [  nn.Conv2d(32, 1, 7, stride=1, padding=3),
                     nn.Sigmoid() ]
